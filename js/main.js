0ce6a741baf1405c03cb7c6fc78a4b8084bf779c
phplpsummer <sh052100@naver.com>
2021-09-17 15:37:03 +0900

    jQuery 완성 1차

--- js/main.js
+++ js/main.js
@@ -1,103 +1,56 @@
+/*
+1. next버튼 클릭
+2. .list marginLeft: -200% 이동
+3. 이동후에 .list marginLeft: -100%(원상태)로 변경  -> 앞에 하나가 나와있는 상태로 원복
+4. 맨앞의 li를 뜯어서 뒷부분으로 보내버린다.
+  : 선택자.appendTo(부모요소);  선택요소의 부모요소 안쪽에서 제일 뒤에 배치
+    <-> 선택자.prependTo(부모요소);  선택요소의 부모요소 안쪽에서 제일 앞에 배치
+
+*/
 // DOM 캐싱
-const $box1 = $("#box1");
-const $box2 = $("#box2");
-const $top = $box1.find(".top");
-const $right = $box1.find(".right");
-const $bottom = $box1.find(".bottom");
-const $left = $box1.find(".left");
-const $content = $("#content");
-const $close1 = $content.find(".btn_close1");
-const $close2 = $box2.find(".btn_close2");
-const $btn1 = $(".btn1");
-const $btn2 = $(".btn2");
+const $list = $(".list");
+let enableClick = true;
 
-let isDone = true;
+let len = $list.find("li").length;
+console.log(len);
 
 
-//이벤트 바인딩
-$btn1.on("click",function(e){
+// 이벤트 바인딩
+$(".list li").last().prependTo($list); //화면1을 맨처음으로 내보이기
+
+
+$list.css({                 //개수에따라 변경하기
+    width: 100 * len + "%",
+    height: "100%"
+});
+$(".list li").css({
+    width: 100 / len + "%",
+    height: "100%"
+})
+
+
+$(".next").on("click",function(e){
     e.preventDefault();
 
-    let isOn = $(this).hasClass("on");
-    if(isOn) return;
-
-    if(isDone){
-        closeBox2();
-        openBox1();
-        $(".btns li a").removeClass("on");
-        $(this).children("a").addClass("on");
-
-        isDone = false;
+    if(enableClick){
+        enableClick = false;
+        $list.animate({marginLeft:'-200%'},1000,function(){
+            $list.css({marginLeft:"-100%"});
+            $(".list li").first().appendTo($list);
+            enableClick = true;
+        });
     }
 });
 
-$close1.on("click",function(e){
+$(".prev").on("click",function(e){
     e.preventDefault();
 
-    closeBox1();
-    $(".btns li a").removeClass("on");
-});
-
-
-$btn2.on("click",function(e){
-    e.preventDefault();
-
-    let isOn = $(this).hasClass("on");
-    if(isOn) return;
-
-    if(isDone){
-        closeBox1();
-        openBox2();
-        $(".btns li a").removeClass("on");
-        $(this).children("a").addClass("on");
-
-        isDone = false;
+    if(enableClick){
+        enableClick = false;
+        $list.animate({marginLeft:'0%'},1000,function(){
+            $list.css({marginLeft:"-100%"});
+            $(".list li").last().prependTo($list);
+            enableClick = true;
+        });        
     }
 });
-
-$close2.on("click",function(e){
-    e.preventDefault();
-    closeBox2();
-    $(".btns li a").removeClass("on");
-});
-
-
-//함수 정의
-function openBox1(){
-    $top.animate({width: "100%"},1000);
-    $bottom.animate({width: "100%"},1000,function(){
-        $right.animate({height: "100%"},1000);
-        $left.animate({height: "100%"},1000,function(){
-            $content.fadeIn(1000,function(){
-                $close1.animate({opacity: 1, right: 20},500,function(){
-                    isDone = true;
-                });
-            });
-        });
-    });
-}
-
-function closeBox1(){
-    $close1.animate({opacity:0,right: "-20px"},500);
-    $content.fadeOut(500,function(){
-        $top.animate({width: "0%"},500);
-        $bottom.animate({width:"0%"},500,function(){
-            $right.animate({height:"0%"},500);
-            $left.animate({height:"0%"},500);
-        });
-    });
-}
-
-function openBox2(){
-    $box2.animate({height:"400px",marginTop:"-200px"},1000,function(){
-        $close2.animate({opacity:1},500,function(){
-            isDone = true;
-        });
-    });
-}
-
-function closeBox2(){
-    $close2.animate({opacity:0},500,function(){
-        $box2.animate({height:0,marginTop:0},500);
-    });
-}
